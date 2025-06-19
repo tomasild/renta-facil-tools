cc17138e43ed49ff3b00e1de1cbc26cd2b0bbc82
tomas.ild <tomas@groovelist.co>
2025-06-19 17:41:09 -0400

    fix vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/renta-facil-tools/", 
   server: {
     host: "::",
     port: 8080,
