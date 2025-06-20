7f4cbf84a14b6683b1839d4c59d9e5e91565c835
tomas.ild <tomas@groovelist.co>
2025-06-19 20:49:24 -0400

    fff

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "https://tomasild.github.io/renta-facil-tools/",
   server: {
     host: "::",
     port: 8080,
