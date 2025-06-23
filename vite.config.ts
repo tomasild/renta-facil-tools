4de912e008862bb37c94b5e661f794eeeed00ccf
tomas.ild <tomas@groovelist.co>
2025-06-23 15:16:28 -0400

    asdsad

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { componentTagger } from 'lovable-tagger';
 
-<<<<<<< HEAD
-export default defineConfig(({ command, mode }) => ({
-  // DEV: '/', BUILD: '/renta-facil-tools/'
-  base: command === 'serve' ? '/' : '/renta-facil-tools/',
-  server: {
-    host: '0.0.0.0', // Cambia '::' a '0.0.0.0' para permitir conexiones externas
-    port: 8080,
-  },
-  build: {
-    outDir: 'docs',
-    assetsDir: 'assets',
-    sourcemap: true, // Activa la generación de sourcemaps para debugging
-    emptyOutDir: true,
-  },
-  plugins: [
-    react(),
-    // Taggea componentes solo en dev
-    command === 'serve' && componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-}));
-=======
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production';
@@ -53,4 +28,4 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
->>>>>>> acc961cbb6b97eb8f668219e64727fca610c728c
+
